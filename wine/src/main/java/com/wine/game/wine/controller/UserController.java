f038c8a09a42a2e02f61fcd47747ef244e9058ed
java_fpc <604772046@qq.com>
2022-03-14 16:20:20 +0800

    新增 springboot小程序工具类。 整合成功

--- wine/src/main/java/com/wine/game/wine/controller/UserController.java
+++ wine/src/main/java/com/wine/game/wine/controller/UserController.java
@@ -3,7 +3,7 @@ package com.wine.game.wine.controller;
 import java.util.Arrays;
 import java.util.Map;
 
-import me.chanjar.weixin.common.bean.WxAccessToken;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -60,6 +60,8 @@ public class UserController {
     @RequestMapping("/save")
     //@RequiresPermissions("wine:user:save")
     public R save(@RequestBody UserEntity user){
+        // wxMaUserServiceImpl.getUserInfo();
+
 		userService.save(user);
 
         return R.ok();
--- wine/src/test/java/com/wine/game/wine/WineApplicationTests.java
+++ wine/src/test/java/com/wine/game/wine/WineApplicationTests.java
@@ -1,13 +1,36 @@
 package com.wine.game.wine;
 
+import cn.binarywang.wx.miniapp.api.WxMaSchemeService;
+import cn.binarywang.wx.miniapp.api.WxMaSecCheckService;
+import cn.binarywang.wx.miniapp.api.WxMaService;
+import me.chanjar.weixin.common.bean.WxAccessToken;
+import me.chanjar.weixin.common.error.WxErrorException;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class WineApplicationTests {
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Autowired
+    private WxMaService wxMaService;
+
     @Test
     void contextLoads() {
+        // redisTemplate.opsForValue().set("1","3");
+        try {
+            // String accessToken = wxMaService.getAccessToken();
+            boolean aa = wxMaService.getSecCheckService().checkMessage("特3456书yuuo莞6543李zxcz蒜7782法fgnv级");
+            boolean bb = wxMaService.getSecCheckService().checkMessage("草拟吗");
+            System.out.println("aa---->"+aa+"bb---->"+bb);
+        } catch (WxErrorException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
